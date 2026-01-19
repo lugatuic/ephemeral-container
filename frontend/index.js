44dee9352154b21dc9a99c0faf4f0697790ed722
Marcin Gorski <mgorski760@gmail.com>
2026-01-19 14:31:27 -0600

    Adding current frontend folder. Need to troubleshoot 405 error with login in

---
+++ frontend/index.js
@@ -0,0 +1,44 @@
+
+function launch() {
+  console.log("attempt to login");
+  const netid = document.getElementById("NetID").value;
+  const button = document.getElementById("login-button");
+  if (!netid.trim()) {
+    alert("Please enter a valid NetID");
+    return;
+  }
+
+  button.disabled = true;
+  button.textContent = "Launching..."
+
+  fetch("doom.acmuic.org/api/launch", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ netid })
+  })
+  .then(res => {
+    if(!res.ok){
+        throw new Error(`HTTP error: ${res.status}`);
+    } else{
+        res.json();
+    }
+  })
+  .then(data => {
+    if (data.success) {
+      document.getElementById("status").innerHTML = `Environment launched. <a href="${data.url}" target="_blank">Click here to access</a>`;
+    } else {
+      document.getElementById("status").textContent = "Error: " + data.message;
+    }
+  })
+  .catch(err => {
+    document.getElementById("status").textContent = "Something went wrong.";
+    button.textContent = "Launch"
+    console.error(err);
+  })
+  .finally(() => {
+    button.disabled = false;
+    button.textContent = "Activate";
+  });
+}
