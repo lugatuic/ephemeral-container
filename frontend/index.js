a2518492f92f57043660ee2243d652295c865f47
Marcin Gorski <mgorski760@gmail.com>
2026-01-31 20:52:00 -0600

    Finished the dropdown and rest of the frontend UI

--- frontend/index.js
+++ frontend/index.js
@@ -3,11 +3,13 @@ function launch() {
   console.log("attempt to login");
   const netid = document.getElementById("NetID").value;
   const button = document.getElementById("login-button");
-  if (!netid.trim()) {
-    alert("Please enter a valid NetID");
+  netid.trim();
+
+  if(template_id == -1 || netid == null){
+    document.getElementById("status").innerHTML = `<span id="fail-label">[FAILED]</span> Please enter your NetID and select a container.`;
     return;
   }
-
+  
   button.disabled = true;
   button.textContent = "Launching..."
   document.getElementById("status").textContent = "";
@@ -17,7 +19,7 @@ function launch() {
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({ netid })
+    body: JSON.stringify({ netid, template_id})
   })
   .then(res => {
     if(!res.ok){
@@ -46,55 +48,49 @@ function launch() {
 }
 
 
-
 //TO DO: API call that returns all available types of containers for users
-
-//Everything is hardcoded until otherwise.
-let container_types = ["Default Debian Linux", "Linux Week 2026"];
-let container_id = [];
 
 //[Key: Name of Container, Value: value for launch]
 const containers = new Map();
 containers.set("Default Debian Linux", 100);
 containers.set("Linux Week 2026", 200);
-
-let selected_id = -1; //Nothing selected. 
+containers.set("TEST ID=142", 142);
+let template_id = -1; //Nothing selected. 
 
 const dropdownButton = document.getElementById("dropdown-button");
+const dropdownText = document.getElementById("button-text");
+const dropdownIcon = document.getElementById("dropdown-icon");
 const dropdownList = document.getElementById("dropdown-list");
 const selectedValue = document.getElementById('selectedValue');
-
-let isActive = false;
 
 //Populate containers within the dropdown.
 containers.forEach((id, name) => {
     const div = document.createElement('div');
     div.className = 'dropdown-item';
     div.textContent = name;
-    div.onclick = () => selectItem(item);
+    div.onclick = () => selectItem(name);
     dropdownList.appendChild(div);
-    console.log("Container Made");
 });
 
 //Toggle dropdown
 dropdownButton.onclick = (e) => {
   e.stopImmediatePropagation(); //Prevent immediate triggering of the closing.
   dropdownList.classList.toggle('open'); 
-  dropdownButton.classList.toggle('close');
+  dropdownIcon.classList.toggle('active');
 }
 
 //Selecting a item from the dropbox
-function selectItem(item){
-  dropdownButton.textContent = item;
-  selectedValue.textContent = item;
-  selected_id = containers.get(`${selectedValue.textContent}`);
+function selectItem(name){
+  dropdownText.textContent = name;
+  template_id = containers.get(name);
   dropdownList.classList.remove('open');
+  dropdownIcon.classList.remove('active');
 }
 
 document.addEventListener('click', (e) => {
     if(!dropdownList.contains(e.target)){
       dropdownList.classList.remove('open');
-      dropdownButton.classList.remove('close');
+      dropdownIcon.classList.remove('active');
     }
 });
 
